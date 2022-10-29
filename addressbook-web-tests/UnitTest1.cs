adc822b639d51088377a109907dbf75cc4c0709e
nastyaraduga <mosnastya1991@mail.ru>
2022-10-29 21:53:08 +0200

    Проект создан

---
+++ addressbook-web-tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace addressbook_web_tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
